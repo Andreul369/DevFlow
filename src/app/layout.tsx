32333aced999dd91fcafcec979e13752531ba558
andreibojor <bojor.andrei@gmail.com>
2024-01-05 14:24:58 +0200

    Fix bug in login functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,25 @@ const spaceGrotesk = Space_Grotesk({
   weight: ['300', '400', '500', '600', '700'],
   variable: '--font-spaceGrotesk',
 });
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk',
+});
 
 export const metadata: Metadata = {
+  title: 'DevFlow - built with Next.js 14',
+  description:
+    'A community driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  },
   title: 'DevFlow - built with Next.js 14',
   description:
     'A community driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.',
@@ -28,6 +45,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500',
+        },
+      }}
+    >
     <ClerkProvider
       appearance={{
         elements: {
