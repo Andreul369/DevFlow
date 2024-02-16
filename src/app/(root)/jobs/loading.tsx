b381cbfb836c6415a88ddeb54b57315c91ed24bc
andreibojor <bojor.andrei@gmail.com>
2024-02-16 09:16:34 +0200

    adding loadings and jobs page + bump

---
+++ src/app/(root)/jobs/loading.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const JobsLoading = () => {
+  return <div>JobsLoading</div>;
+};
+
+export default JobsLoading;
---
+++ src/app/(root)/jobs/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Filter from '@/components/shared/filter';
+import LocalSearchbar from '@/components/shared/search/local-searchbar';
+import { HomePageFilters } from '@/constants/filters';
+
+const JobsPage = () => {
+  return (
+    <>
+      <div className='flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center'>
+        <h1 className='h1-bold text-dark100_light900'>Jobs</h1>
+      </div>
+
+      <div className='mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center'>
+        <LocalSearchbar
+          route='/'
+          iconPosition='left'
+          imgSrc='/assets/icons/search.svg'
+          placeholder='Search for questions'
+          otherClasses='flex-1'
+        />
+
+        <Filter
+          filters={HomePageFilters}
+          otherClasses='min-h-[56px] sm:min-w-[170px]'
+          containerClasses='hidden max-md:flex'
+        />
+      </div>
+    </>
+  );
+};
+
+export default JobsPage;
